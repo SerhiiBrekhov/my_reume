96d5064b1d2f957698eff75b4165bea4dfdc1cfd
Сергій Брехов <sergbr78@gmail.com>
2021-04-06 22:35:14 +0300

    serhii
    fix files

--- js/sliders.js
+++ js/sliders.js
@@ -1,20 +1,23 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-    speed: 1000.
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+  
+    speed: 1000,
+    effect: 'slide',
+    grabCursor: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
